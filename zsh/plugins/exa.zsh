4e5831c00252efc944bc0febaad577b9dc7db331
Gerson Gallo <ggallovalle@gmail.com>
2020-12-15 10:09:42 -0600

    add aliases for more ls commands

--- zsh/plugins/exa.zsh
+++ zsh/plugins/exa.zsh
@@ -2,7 +2,8 @@ typeset -A EXA
 EXA[tree]="exa -T -L 3 --git-ignore --color always --group-directories-first"
 if command -v exa&>/dev/null; then
   alias ls='exa --group-directories-first'
-  alias ll='exa --long --group-directories-first'
+  alias ll='exa --long --group-directories-first --header'
+  alias lla='exa --long --group-directories-first --all --header'
   alias la='exa --all --group-directories-first'
   alias ld='exa --only-dirs'
 fi
