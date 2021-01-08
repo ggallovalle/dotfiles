f84716fe9d7ab19692f46db3ccc0400aa50a9db7
Gerson Gallo <ggallovalle@gmail.com>
2021-01-08 16:01:21 -0600

    feat(fish): wphp completion make use of pkginfo own script made in python

--- fish/completions/wphp.fish
+++ fish/completions/wphp.fish
@@ -2,45 +2,11 @@ function __composer_required_packages
     set -l base (find-up composer.json)
     test -z $base
     and return 1
-
-    echo "
-require 'json'
-file = File.read '$base/composer.json'
-data = JSON.parse file
-packages = data['require'].keys + data['require-dev'].keys
-packages.delete 'php'
-puts packages
-      " | ruby
-end
-
-function __composer_required_packages_prod
-    set -l base (find-up composer.json)
-    test -z $base
-    and return 1
-
-    echo "
-require 'json'
-file = File.read '$base/composer.json'
-data = JSON.parse file
-packages = data['require'].keys
-packages.delete 'php'
-puts packages
-      " | ruby
-end
-
-function __composer_required_packages_dev
-    set -l base (find-up composer.json)
-    test -z $base
-    and return 1
-
-    echo "
-require 'json'
-file = File.read '$base/composer.json'
-data = JSON.parse file
-packages = data['require-dev'].keys
-packages.delete 'php'
-puts packages
-      " | ruby
+    set -l where all
+    if test -n "$argv"
+        set where $argv
+    end
+    pkginfo -l php -f $base -d $where -c
 end
 
 function __composer_inside_project
@@ -49,7 +15,6 @@ function __composer_inside_project
     and return 1
     or return 0
 end
-
 
 function __composer_needs_command
     set -l cmd (commandline -opc)
@@ -70,5 +35,5 @@ complete -c $this -n '__composer_inside_project; and __composer_needs_command' -
 complete -c $this -n '__composer_inside_project; and __composer_needs_command' -a remove-dev -d 'remove dev package'
 
 # remove
-complete -f -c $this -n '__fish_seen_subcommand_from remove' -a '(__composer_required_packages_prod)'
-complete -f -c $this -n '__fish_seen_subcommand_from remove-dev' -a '(__composer_required_packages_dev)'
+complete -f -c $this -n '__fish_seen_subcommand_from remove' -a '(__composer_required_packages prod)'
+complete -f -c $this -n '__fish_seen_subcommand_from remove-dev' -a '(__composer_required_packages dev)'
