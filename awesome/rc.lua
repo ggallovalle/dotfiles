c7cb0ab024458fd877888e9f80a58dc66c1e9cd3
Gerson Gallo <ggallovalle@gmail.com>
2021-02-09 13:06:27 -0600

    chore(awesomewm): max layout is the last one

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -66,11 +66,11 @@ tmux_cmd = terminal .. "-e tmux attach"
 modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts = {awful.layout.suit.tile, awful.layout.suit.tile.bottom, awful.layout.suit.fair,
 -- awful.layout.suit.fair.horizontal,
 -- awful.layout.suit.spiral,
 -- awful.layout.suit.spiral.dwindle,
-                        awful.layout.suit.max, awful.layout.suit.magnifier}
+awful.layout.layouts = {awful.layout.suit.tile, awful.layout.suit.tile.bottom, awful.layout.suit.fair,
+                        awful.layout.suit.magnifier, awful.layout.suit.max}
 
 -- beautiful.hotkeys_bg = "#050505"
 -- beautiful.hotkeys_label_bg = "#050505"
