b675f94ec6ad1ab04b68218fff8a9aa08450c77d
Gerson Gallo <ggallovalle@gmail.com>
2021-04-27 08:16:47 -0600

    fix omf startup error

--- fish/config.fish
+++ fish/config.fish
@@ -36,6 +36,12 @@ end
 # END ]]
 
 # [[ this two needs to always be the last two
-load-component omf
+# Omf needs to be inline in config.fish or it gives some nasty errors
+set -x OMF_CONFIG "$__fish_config_dir/omf"
+set -x OMF_PATH "$XDG_DATA_HOME/omf"
+# Load Oh My Fish configuration.
+test -d $OMF_PATH
+or git clone https://github.com/oh-my-fish/oh-my-fish $OMF_PATH
+source $OMF_PATH/init.fish
 load-component starship
 # END ]]
