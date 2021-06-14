00e12b42354ae9a42c5462970fa972c42eb44103
Gerson Gallo <ggallovalle@gmail.com>
2021-06-14 09:12:00 -0600

    feat: keychain compomnent updated and added @nestj/graphql to clones packages

--- fish/components/keychain.fish
+++ fish/components/keychain.fish
@@ -1,11 +1,9 @@
 if command -q keychain
     # TODO make everything in an array and eval only once
-    test -f $HOME/.ssh/github_ggallovalle
-    and keychain --quiet --eval github_ggallovalle | source
+    test -f $HOME/.ssh/contollo_rsa
+    and keychain --quiet --eval contollo_rsa | source
     test -f $HOME/.ssh/id_rsa
     and keychain --quiet --eval id_rsa | source
-    test -f $HOME/.ssh/outlook_rsa
-    and keychain --quiet --eval outlook_rsa | source
 else
     msg -w "keychain not installed"
 end
