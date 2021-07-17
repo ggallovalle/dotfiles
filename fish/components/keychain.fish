2eb5cbe77b48c8e18e66f7778af05e2a3e73e278
Gerson Gallo <ggallovalle@gmail.com>
2021-07-16 19:50:53 -0600

    feat: remove contollo ssh key

--- fish/components/keychain.fish
+++ fish/components/keychain.fish
@@ -1,7 +1,5 @@
 if command -q keychain
     # TODO make everything in an array and eval only once
-    test -f $HOME/.ssh/contollo_rsa
-    and keychain --quiet --eval contollo_rsa | source
     test -f $HOME/.ssh/id_rsa
     and keychain --quiet --eval id_rsa | source
 else
