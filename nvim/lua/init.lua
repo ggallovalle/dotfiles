6a759cb5b3316a74bfe134527d66f77edd47b75e
Gerson Gallo <ggallovalle@gmail.com>
2021-08-18 20:07:54 -0600

    feat: move stuff to own "pacakge" to avoid name colisions

--- nvim/lua/init.lua
+++
@@ -1,6 +0,0 @@
----@diagnostic disable-next-line: undefined-global
-local mapping = require("_mapping")
-local options = require("_options")
-
-options()
-mapping()
---
+++ nvim/lua/kbroom-config/init.lua
@@ -0,0 +1,5 @@
+local remaps = require("kbroom-config.remaps")
+local options = require("kbroom-config.options")
+
+options()
+remaps()
