ec737c24c67f1b3db9b48dbb564dbb3c6d5174b7
Gerson Gallo <ggallovalle@gmail.com>
2025-09-08 04:44:07 -0600

    feat: remove old nvim configuration

--- nvim/lua/kbroom-config/plugins/vim-highlightedyank.lua
+++
@@ -1,8 +0,0 @@
-local opts = require("kbroom-config.options")
-
-return {
-    "machakann/vim-highlightedyank",
-    config = function()
-        opts.global({ highlightedyank_whighlight_duration = 100 })
-    end,
-}
